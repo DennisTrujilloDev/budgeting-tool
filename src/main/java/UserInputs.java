dfe192f995f49c11922c55dcaa83083469daf596
ndhakal <nabindhakall@hotmail.com>
2022-08-09 09:35:19 -0700

    Implemented Getters and Setters for UserInputs.

--- src/main/java/UserInputs.java
+++ src/main/java/UserInputs.java
@@ -9,4 +9,67 @@ public class UserInputs {
   double monthlyDeptPayment;
   double monthlyExpenses;
 
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getIndividualOrFamily() {
+    return individualOrFamily;
+  }
+
+  public void setIndividualOrFamily(String individualOrFamily) {
+    this.individualOrFamily = individualOrFamily;
+  }
+
+  public String getHomeowner() {
+    return homeowner;
+  }
+
+  public void setHomeowner(String homeowner) {
+    this.homeowner = homeowner;
+  }
+
+  public double getRent() {
+    return rent;
+  }
+
+  public void setRent(double rent) {
+    this.rent = rent;
+  }
+
+  public double getMortgage() {
+    return mortgage;
+  }
+
+  public void setMortgage(double mortgage) {
+    this.mortgage = mortgage;
+  }
+
+  public double getAnnualIncome() {
+    return annualIncome;
+  }
+
+  public void setAnnualIncome(double annualIncome) {
+    this.annualIncome = annualIncome;
+  }
+
+  public double getMonthlyDeptPayment() {
+    return monthlyDeptPayment;
+  }
+
+  public void setMonthlyDeptPayment(double monthlyDeptPayment) {
+    this.monthlyDeptPayment = monthlyDeptPayment;
+  }
+
+  public double getMonthlyExpenses() {
+    return monthlyExpenses;
+  }
+
+  public void setMonthlyExpenses(double monthlyExpenses) {
+    this.monthlyExpenses = monthlyExpenses;
+  }
 }

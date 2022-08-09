f0b5b67521ed694b65e3850cd2dc076af69ca5a5
ndhakal <nabindhakall@hotmail.com>
2022-08-08 17:16:13 -0700

    Added code to get some user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
+
+  private static UserProfile userProfile;
+  private static boolean isHomeOwner;
+  private static Integer mortgage;
+  private static Integer rent;
+  private static Integer annualIncome;
+
 
   public static void main(String[] args) {
 
@@ -12,5 +20,81 @@ public class Main {
     System.out.println("Hello, " + name);
 
     System.out.println("Are you an individual or a family? Type I for Individual, F for Family.");
+
+    while(true) {
+      String individualOrFamily = sc.nextLine();
+      if(individualOrFamily.equalsIgnoreCase("i")) {
+        userProfile = UserProfile.INDIVIDUAL;
+        break;
+      } else if (individualOrFamily.equalsIgnoreCase("f")) {
+        userProfile = UserProfile.FAMILY;
+        break;
+      } else {
+        System.out.println("Please input correct option. I for Individual, F for Family.");
+      }
+    }
+    System.out.println("User Profile: " + userProfile);
+
+    System.out.println("Are you a home owner? Y for Yes, N for No.");
+    String homeOwner = sc.nextLine();
+
+    while(true) {
+      if(homeOwner.equalsIgnoreCase("y")) {
+        isHomeOwner = true;
+        break;
+      } else if (homeOwner.equalsIgnoreCase("n")) {
+        isHomeOwner = false;
+        break;
+      } else {
+        System.out.println("Please input correct option. Y for Yes, N for No. Are you a home owner?");
+      }
+    }
+
+    //If the user is a home owner, ask how much mortgage do they pay
+    if(isHomeOwner) {
+      while (true) {
+        System.out.println("How much do you pay in mortgage each month?");
+        String mortgageStr = sc.nextLine();
+
+        try {
+          mortgage = Integer.parseInt(mortgageStr);
+          break;
+        } catch (NumberFormatException e) {
+          System.out.println("Incorrect Input. Please enter valid number.");
+        }
+      }
+      System.out.println("Monthly Mortgage: " + mortgage);
+    }
+
+    //If the user is not a home owner (renter), ask how much rent do they pay
+    if(!isHomeOwner) {
+      System.out.println("How much rent do yoy pay each month?");
+      String rentStr = sc.nextLine();
+
+      while(true) {
+        try {
+          rent = Integer.parseInt(rentStr);
+          break;
+        } catch (NumberFormatException e) {
+          System.out.println("Incorrect Input. Please enter valid number.");
+        }
+      }
+      System.out.println("Monthly Rent: " + rent);
+    }
+
+    // Ask user for their annual income
+    System.out.println("What's your annual income?");
+    String annualIncomeStr = sc.nextLine();
+
+    while (true) {
+      try {
+        annualIncome = Integer.parseInt(annualIncomeStr);
+        break;
+      } catch (NumberFormatException e){
+        System.out.println("Incorrect Input. Please enter valid number.");
+      }
+    }
+    System.out.println("Annual Income: " + annualIncome);
+
   }
 }
---
+++ src/main/java/UserProfile.java
@@ -0,0 +1,4 @@
+public enum UserProfile {
+  INDIVIDUAL,
+  FAMILY
+}

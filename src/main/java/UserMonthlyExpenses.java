fd53d26d28851439cc3cdbee826df75c825bbc79
ndhakal <nabindhakall@hotmail.com>
2022-08-09 09:23:57 -0700

    Implemented getters and setters for fields of UserMonthlyExpenses

--- src/main/java/UserMonthlyExpenses.java
+++ src/main/java/UserMonthlyExpenses.java
@@ -9,5 +9,67 @@ public class UserMonthlyExpenses {
   double gas;
   double miscellaneous; // i.e shopping and any other emergency expenses average in a month.
 
+  public double getElectricBill() {
+    return electricBill;
+  }
 
+  public void setElectricBill(double electricBill) {
+    this.electricBill = electricBill;
+  }
+
+  public double getInternetBill() {
+    return internetBill;
+  }
+
+  public void setInternetBill(double internetBill) {
+    this.internetBill = internetBill;
+  }
+
+  public double getWaterBill() {
+    return waterBill;
+  }
+
+  public void setWaterBill(double waterBill) {
+    this.waterBill = waterBill;
+  }
+
+  public double getPhoneBill() {
+    return phoneBill;
+  }
+
+  public void setPhoneBill(double phoneBill) {
+    this.phoneBill = phoneBill;
+  }
+
+  public double getGroceries() {
+    return groceries;
+  }
+
+  public void setGroceries(double groceries) {
+    this.groceries = groceries;
+  }
+
+  public double getCarInsurance() {
+    return carInsurance;
+  }
+
+  public void setCarInsurance(double carInsurance) {
+    this.carInsurance = carInsurance;
+  }
+
+  public double getGas() {
+    return gas;
+  }
+
+  public void setGas(double gas) {
+    this.gas = gas;
+  }
+
+  public double getMiscellaneous() {
+    return miscellaneous;
+  }
+
+  public void setMiscellaneous(double miscellaneous) {
+    this.miscellaneous = miscellaneous;
+  }
 }

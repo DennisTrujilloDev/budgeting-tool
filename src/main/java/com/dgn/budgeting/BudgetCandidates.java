0c696cc803a5ae52f43757e71e3b4c72eb8569cc
DennisTrujilloDev <dennis.joel.trujillo@gmail.com>
2022-08-09 15:41:39 -0400

    added advice for leftover money

---
+++ src/main/java/com/dgn/budgeting/BudgetCandidates.java
@@ -0,0 +1,86 @@
+package com.dng;
+
+import com.dng.Debt;
+
+//I think in the business methods below we can get rid of "Object"
+//and write the method's return type instead
+
+public class BudgetCandidates {
+
+  public static final double EMERGENCY_FUND_CONSTANT = 0.10;
+  public static final double SAVINGS_CONSTANT = 0.15;
+  public static final double MISCELLANEOUS_CONSTANT = 0.40;
+  public static final double FOOD_CONSTANT = 0.25;
+
+  //fields
+  double income;
+  Debt debt;
+  double debtplusExpenses;
+  double budgetAmount;
+
+  public BudgetCandidates(double income, Debt debt) {
+    this.income = income;
+    this.debt = debt;
+  }
+
+  public Debt getDebt() {
+    return debt;
+  }
+
+  public void setDebt(double debt) {
+    this.debtplusExpenses = debt;
+  }
+
+  public double budgetAmount() {
+    budgetAmount = Math.abs(debtplusExpenses - income);
+    return budgetAmount;
+  }
+
+  //Business Methods
+  //Method to calculate recommended amount for emergency fund
+  public Object calculateEmergencyFund() {
+    double amount = budgetAmount() * EMERGENCY_FUND_CONSTANT;
+    return amount;
+
+  }
+
+  //Method to calculate recommended amount for savings
+  public Object calculateSavings() {
+    double amount = budgetAmount() * SAVINGS_CONSTANT;
+    return amount;
+  }
+
+  //Method to calculate recommended amount for miscellaneous
+  public Object calculateMiscellanous() {
+    double amount = budgetAmount() * MISCELLANEOUS_CONSTANT;
+    return amount;
+  }
+
+  //Method to calculate recommended amount for food
+  public Object calculateFoodBudget() {
+    double amount = budgetAmount() * FOOD_CONSTANT;
+    return amount;
+  }
+
+
+  void availableBudgetNotification() {
+
+    if (UserInput.getTotalAvailableBudget() < 0) {
+      //proly have to add class name before the property name
+      System.out.println("You are at risk of falling into debt; please see a financial advisor.");
+    } else if (totalAvailableBudget < 1000) {
+      System.out.println(
+          "You may need to cut down on your expenses. We recommend downloading an app to help keep track of — and minimize — your miscellaneous expenses.");
+    } else {
+      System.out.println("Great job budgeting! Keep it up!");
+    }
+  }
+
+  void leftoverMoneyAfterBudgeting(){
+    double leftovers = 100 -((EMERGENCY_FUND_CONSTANT + SAVINGS_CONSTANT + MISCELLANEOUS_CONSTANT + FOOD_CONSTANT) * 100);
+    System.out.printf("We suggest investing the remaining %f%n in the stock market", leftovers);
+  }
+
+
+}
+
---
+++ src/main/java/com/dgn/budgeting/Printing.java
@@ -0,0 +1,20 @@
+package com.dgn.budgeting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ResourceBundle;
+
+public class Printing{
+//  private Reader reader;
+//  private BufferedReader buffer;
+
+//  private final String BUNDLE_NAME = "messages";
+//  private final ResourceBundle BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+//  private InputStream input;
+
+
+}
+
+

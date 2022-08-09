b1fa72def027d22623f851f7fef4641d30f8d5f3
ndhakal <nabindhakall@hotmail.com>
2022-08-09 09:34:24 -0700

    Defined field for UserInputs.

--- src/main/java/UserInputs.java
+++ src/main/java/UserInputs.java
@@ -1,3 +1,12 @@
 public class UserInputs {
 
+  String name;
+  String individualOrFamily;
+  String homeowner;
+  double rent;
+  double mortgage;
+  double annualIncome;
+  double monthlyDeptPayment;
+  double monthlyExpenses;
+
 }

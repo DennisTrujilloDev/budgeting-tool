886e856600f35054709c8f86dec4e360473e9616
DennisTrujilloDev <dennis.joel.trujillo@gmail.com>
2022-08-10 12:54:43 -0400

    added set up for advice class

---
+++ src/main/java/com/dng/Advice.java
@@ -0,0 +1,18 @@
+package com.dng;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Advice {
+
+    UserMonthlyExpenses debt = new UserMonthlyExpenses();
+    Locale locale = new Locale("en", "US");
+    NumberFormat fmt = NumberFormat.getCurrencyInstance(locale);
+
+
+    BudgetItem budget = new BudgetItem(debt.getMonthlyIncome(), debt.totalMonthlyDebtPayment);
+    String info = fmt.format(budget.budgetAmount());
+
+
+    
+}

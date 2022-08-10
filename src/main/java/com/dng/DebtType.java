517467f1c5ece00819f481e79962e457831a1688
ndhakal <nabindhakall@hotmail.com>
2022-08-10 13:27:51 -0700

    toSting Implemented for DeptTypeEnum

--- src/main/java/com/dng/DebtType.java
+++ src/main/java/com/dng/DebtType.java
@@ -1,8 +1,25 @@
 package com.dng;
 
 public enum DebtType {
-  CAR_LOAN,
-  MEDICAL_BILLS,
-  STUDENT_LOAN,
-  CREDIT_CARDS
+  CAR_LOAN               ("Car Loan"),
+  MEDICAL_BILLS          ("Medical bills"),
+  STUDENT_LOAN           ("Student Loans"),
+  CREDIT_CARDS           ("Credit Cards");
+
+ String debtName;
+
+ DebtType(String debtName) {
+   this.debtName = debtName;
+
+ }
+
+  @Override
+  public String toString() {
+    return debtName;
+  }
 }
+
+
+
+
+

f454eaf5b278417f80b51d431a41546688e0e82d
Gabekross <olugabriel80@gmail.com>
2022-08-10 15:26:15 -0700

    Deleted UseInteraction Class

--- src/main/java/com/dng/DebtType.java
+++ src/main/java/com/dng/DebtType.java
@@ -1,13 +1,30 @@
+//package com.dng;
+//
+//public enum DebtType {
+//
+//  CAR_LOAN,
+//  MEDICAL_BILLS,
+//  STUDENT_LOAN,
+//  PERSONAL_LOAN,
+//  CREDIT_CARDS,
+//  ALIMONY,
+//
+//
+//}
+
 package com.dng;
-
 public enum DebtType {
+  CAR_LOAN               ("Car Loan"),
+  MEDICAL_BILLS          ("Medical Bills"),
+  STUDENT_LOAN           ("Student Loans"),
+  CREDIT_CARDS           ("Credit Cards");
+  String debtName;
+  DebtType(String debtName) {
+    this.debtName = debtName;
+  }
 
-  CAR_LOAN,
-  MEDICAL_BILLS,
-  STUDENT_LOAN,
-  PERSONAL_LOAN,
-  CREDIT_CARDS,
-  ALIMONY,
-
-
+  @Override
+  public String toString() {
+    return debtName;
+  }
 }
--- src/main/java/com/dng/ExpenseType.java
+++ src/main/java/com/dng/ExpenseType.java
@@ -2,13 +2,31 @@ package com.dng;
 
 public enum ExpenseType {
 
-  ELECTRIC_BILL,
-  INTERNET_BILL,
-  WATER_BILL,
-  PHONE_BILL,
-  GROCERIES,
-  CAR_INSURANCE,
-  GAS,
-  MISCELLANEOUS // i.e. Shopping and Emergency expenses
+//  ELECTRIC_BILL,
+//  INTERNET_BILL,
+//  WATER_BILL,
+//  PHONE_BILL,
+//  GROCERIES,
+//  CAR_INSURANCE,
+//  GAS,
+//  MISCELLANEOUS // i.e. Shopping and Emergency expenses
+
+  ELECTRIC_BILL            ("Electric Bill"),
+  INTERNET_BILL            ("Internet Bill"),
+  WATER_BILL               ("Water Bill"),
+  PHONE_BILL               ("Phone Bill"),
+  GROCERIES                ("Groceries"),
+  CAR_INSURANCE            ("Car Insurance"),
+  GAS                      ("Gas"),
+  MISCELLANEOUS            ("Miscellaneous");                                      // i.e. Shopping and Emergency expenses
+  String expenseName;
+  ExpenseType(String expenseName) {
+    this.expenseName = expenseName;
+  }
+
+  @Override
+  public String toString() {
+    return expenseName;
+  }
 
 }

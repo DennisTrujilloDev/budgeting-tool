cd3e0c4b000b6ae8abc5938386dc07a608fdd1dc
ndhakal <nabindhakall@hotmail.com>
2022-08-10 07:30:58 -0700

    Correction on UserInput Class

--- src/main/java/com/dng/BudgetCandidates.java
+++ src/main/java/com/dng/BudgetCandidates.java
@@ -21,16 +21,18 @@ public class BudgetCandidates {
 
     if(totalAvailableBudget <= 0) {
       //TO-DO
-      System.out.println("Total Available Budget less than 0. You will need to think hard on how to manage your money.");
+      System.out.println("Your expenses are more higher than your income. You will need to think hard on how to manage your money. \n"
+      + "Find out how the state and federal government can support you financial situation. \n"
+              + "Cut down on your non critical expenses. \n");
     }
     else if (totalAvailableBudget <= monthlyIncome * 0.20) {
       //TO-DO
-      System.out.println("f budget amount is less than a certain threshold,  \n"
+      System.out.println("Your expenses are higher than your income,  \n"
           + "Issue a warning about overspending and maybe cutting down on miscellaneous expenses.\n"
           + "Also, give advise on getting an extra source of income to supplement their current income.");
     }
     else {
-      System.out.println("You are on the right track!");
+      System.out.println("You have good financial health. Be sure to keep your money in Savings for future");
     }
   }
 
